2802169f9a5327f6654a318bfa73cb7bae45e351
Shoji Moto <shmoto1@gmail.com>
2015-10-23 20:48:05 -0400

    Worked on bootstrap, added smooth scrolling

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+$(function(){
+	
+	$('a[href^="#"]').click(function(e){
+		
+		var target = $(this).attr('href');
+		var strip = target.slice(1);
+		var anchor = $("a[name='" + strip +"']");
+		
+		
+		e.preventDefault();
+		
+		$('html, body').animate({
+			
+			scrollTop: anchor.offset().top
+			
+		}, 'slow');
+	});
+});
