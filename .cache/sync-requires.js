ff8c2d2aa60496847ef24a6291523d84ad67d2ed
pianobin <shoji@pianobin.com>
2024-01-12 21:47:22 -0800

    updated files

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/node_modules/gatsby-plugin-offline/app-shell.js"))),
   "component---src-templates-not-found-template-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/src/templates/not-found-template.js"))),
   "component---src-templates-tags-list-template-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/src/templates/tags-list-template.js"))),
   "component---src-templates-categories-list-template-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/src/templates/categories-list-template.js"))),
@@ -13,6 +12,7 @@ exports.components = {
   "component---src-templates-post-template-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/src/templates/post-template.js"))),
   "component---src-templates-tag-template-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/src/templates/tag-template.js"))),
   "component---src-templates-category-template-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/src/templates/category-template.js"))),
-  "component---src-templates-index-template-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/src/templates/index-template.js")))
+  "component---src-templates-index-template-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/src/templates/index-template.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/smoto2/Documents/GitHub/PianoBin.github.io/.cache/dev-404-page.js")))
 }
 
