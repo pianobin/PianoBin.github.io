3a7da9a9fc3db8329ff4d482754f63709195c5eb
pianobin <shoji@pianobin.com>
2024-03-19 16:57:07 -0700

    add purge css

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -195,8 +195,24 @@ export default {
     {
       resolve: `gatsby-plugin-disqus`,
       options: {
-          shortname: `pianobin`
+          shortname: ``
       }
+    },
+    // Add after these plugins if used
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        purgeCSSOptions: {
+          // https://purgecss.com/configuration.html#options
+          // safelist: ['safelist'], // Don't remove this selector
+        },
+        // More options defined here https://purgecss.com/configuration.html#options
+      },
     },
   ],
 };
