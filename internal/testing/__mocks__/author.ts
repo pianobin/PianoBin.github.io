7cb9caccab70e1b82d517e5e5ade50cd29b67025
pianobin <shoji@pianobin.com>
2024-03-19 17:22:12 -0700

    remove test temp

--- internal/testing/__mocks__/author.ts
+++
@@ -1,8 +0,0 @@
-import contacts from "./contacts";
-
-export default {
-  photo: "/photo.png",
-  bio: "Piano tutorials, sheet music, and arrangements.",
-  name: "Pianobin",
-  contacts,
-};
--- src/utils/test-utils/index.ts
+++ src/utils/test-utils/index.ts
@@ -1,5 +1,1 @@
-export {
-  default as renderWithCoilProvider,
-  createSnapshotsRenderer,
-} from "./render-with-coil-provider";
 export { default as getMeta } from "./get-meta";
