f7d08c5446bcea2df177375d6a25fdcb31ff0a54
pianobin <shoji@pianobin.com>
2024-03-19 17:21:53 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-73594095c5bd09f92e36.js"
-  },
-  {
-    "url": "framework-1ab81499b0495128b8ef.js"
+    "url": "webpack-runtime-53c5d6d4af8cec82fcc7.js"
   },
   {
     "url": "styles.4c26b084fa4bc54e7be9.css"
   },
   {
-    "url": "app-3302d79cb5b519a9697d.js"
+    "url": "framework-8a5f28e64bea9162217b.js"
+  },
+  {
+    "url": "app-0aafb33a5cf2bfab93c5.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "42831306a2e4da51745ddfdfd8961147"
+    "revision": "33131156d3846829d2f09ad4a61f61d3"
   },
   {
     "url": "manifest.webmanifest",
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-3302d79cb5b519a9697d.js`))) {
+  if (!resources || !(await caches.match(`/app-0aafb33a5cf2bfab93c5.js`))) {
     return await fetch(event.request)
   }
 
